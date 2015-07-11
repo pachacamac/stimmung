d9e4f5ca68318ed22e4bd4ac6730a3727bad1141
Alex Weil <alex@alexjweil.com>
2015-07-11 19:24:07 +0300

    Added tests for emoticons and fixed dictionary loading test

--- spec/stimmung_spec.rb
+++ spec/stimmung_spec.rb
@@ -1,12 +1,12 @@
 require_relative "../lib/stimmung"
 
 describe Stimmung do
-  
+
   describe '#initialize' do 
     let(:stimmung) {Stimmung.new(File.join(File.dirname(__FILE__), '..', 'lib', 'emoticons.txt'))}
-   
+
     it "can load other dictionaries" do
-      expect(stimmung.words.size).to eq(55)
+      expect(stimmung.words.size).to eq(54)
     end
   end
 
@@ -25,12 +25,20 @@ describe Stimmung do
       expect(stimmung.score('ich hasse dummes beschissenes wetter')).to eq(-0.9388000000000001)
     end
 
+
     context 'when passed a string containing non-letter characters' do
       it 'strips out those characters' do
-        expect(stimmung.score('Das;;ist;;sehr;;traurig;;')).to eq(stimmung.score('Das ist sehr traurig'))
+        expect(stimmung.score('Das; ist? sehr+ traurig!?!')).to eq(stimmung.score('Das ist sehr traurig'))
       end
+
+      it "doesn't remove emoticons, though" do
+        expect(stimmung.score(';)')).to eq(1.0)
+      end
+
+      it "can distinguish between punctuation and emoticons" do
+        expect(stimmung.score('Was macht ein Hacker am See? Piraten!! :D')).to eq(1.004)
+      end
+
     end
-
   end
-
 end
